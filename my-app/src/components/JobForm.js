5d025db91048494b303e5b398ab9ac45e8e0ebdb
Keith Decker <kdecker09@gmail.com>
2018-04-06 13:17:14 -0600

    commit before altering job list

--- my-app/src/components/JobForm.js
+++ my-app/src/components/JobForm.js
@@ -1,19 +1,41 @@
 import React from 'react'
 
 class JobForm extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      title: '',
+      compensation: '',
+      description: ''
+    }
+
+    this.handleChange = this.handleChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+  }
+
+  handleChange(event) {
+    this.setState({value: event.target.value})
+  }
+
+handleSubmit(event) {
+  this.setState({title: title})
+  event.preventDefault()
+}
+
   render() {
   return (
       <aside id="side-bar">
         <h3>Add a Job</h3>
         <form className="job-form">
           <label htmlFor="title">Title</label>
-          <input type="text" className="title"/>
+          <input type="text" className="title" value={this.state.title} onChange={this.handleChange}/>
           <label htmlFor="pay">Compensation</label>
           <input type="text" className="pay"/>
           <label htmlFor="description">Description</label>
           <textarea className="description" rows="8" cols="40"></textarea>
           <input type="submit" className="submit" value="Submit" />
         </form>
+        <div>{this.state.title}</div>
       </aside>
     )
   }
